d5f367028ac67f0cde1219a1a554c0f29e7a2c29
sabarish <sabarish.cs8@gmail.com>
2024-12-14 16:13:52 +0530

    Add logo

---
+++ components/Header.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+function Header() {
+  return (
+    <div className=''>
+        <Image 
+           className="rounded-lg"
+           src=""
+           width={40}
+           height={40}
+           alt="logo"
+           />
+    </div>
+  )
+}
+
+export default Header
