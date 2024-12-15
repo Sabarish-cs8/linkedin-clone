b562aaa15bcc2b13c58ed27dd7dbb065a3f6c5c9
sabarish <sabarish.cs8@gmail.com>
2024-12-15 10:01:55 +0530

    modified : PostForm

--- components/PostForm.tsx
+++ components/PostForm.tsx
@@ -3,13 +3,24 @@
 import { useUser } from "@clerk/nextjs"
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar"
 import { Button } from "./ui/button";
-import { ImageIcon } from "lucide-react";
+import { ImageIcon, XIcon } from "lucide-react";
+import { useRef, useState } from "react";
 
 function PostForm() {
+    const ref = useRef<HTMLFormElement>(null);
+    const fileInputRef = useRef<HTMLFormElement>(null);   
     const { user } = useUser();
+    const [preview , setPreview] = useState<string | null>(null);
+
+    const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>)=>{
+        const file = event.target.files?.[0];
+        if(file){
+            setPreview(URL.createObjectURL(file));
+        }
+    }
   return (
     <div>
-        <form action="">
+        <form ref={ref} action="">
             <div className="flex items-center space-x-2">
                 <Avatar>
                     <AvatarImage src={user?.imageUrl} />
@@ -26,23 +37,42 @@ function PostForm() {
                   className="flex-1 outline-none rounded-full py-3 px-4 border"                   
                 />
 
-                <input type="file" name="image" accept="image/*" hidden />
+                <input 
+                ref={fileInputRef}
+                 type="file" 
+                 name="image" 
+                 accept="image/*" 
+                 hidden 
+                onChange={handleImageChange}
+                />
                 <button type="submit" hidden >
                     Post
                 </button>
             </div>
 
             {/** Preview conditional check */}
+            {preview && (
+                <div className="mt-3">
+                    <img src={preview} alt="Preview" className="w-full object-cover" />
+                </div>
+            )}
 
-            <div>
-                <Button>
+            <div className="flex justify-end mt-2 space-x-2">
+                <Button type="buttton" onClick={()=>fileInputRef.current?.click()}>
                     <ImageIcon className="mr-2" size={16} color="currentColor" />
-                    Add
+                    {preview ? "change" :"Add"} image
                 </Button>
 
                 {/** Add a remove preview button */}
+                {preview && (
+                    <Button variant="outline" type="button" onClick={()=>setPreview(null)}>
+                        <XIcon className="mr-2" size={16} color="currentColor" />
+                        Remove image
+                    </Button>
+                )}
             </div>
         </form>
+        <hr className="mt-2 border-gray-300" />
     </div>
   )
 }
